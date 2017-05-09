c7ae9d4beeb02f7be97480a1bb9e0eed733ec22b
8GutiMu <you@example.com>
2017-05-09 14:43:44 -0500

    archivos

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+var parrafoOrigen = document.getElementsByTagName("p")[0];
+
+var parrafoExtincion = document.getElementsByTagName("p")[1];
+
+var botonOrigen = document.getElementsByTagName("button")[0];
+
+var botonExtincion = document.getElementsByTagName("button")[1];
+
+botonExtincion.addEventListener("click",aparecerParrafo)
+
+function aparecerParrafo(){
+   parrafoOrigenOrigen.visibility = "hidden"
+}
+
